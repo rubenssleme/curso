e6cc885bc418bf3c185bec725e8efd3389178cb8
Rubens Leme <rubenss.leme@gmail.com>
2025-11-19 12:41:43 -0300

    Update pom.xml to include Lombok dependency and set compiler properties

--- src/main/java/com/curso2020/curso/Application.java
+++ src/main/java/com/curso2020/curso/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Ponto de entrada da aplicação Spring Boot.
+ * novo
  */
 @SpringBootApplication
 public class Application {
